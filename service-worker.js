5ca8177f612e63253d7b4ead75d44f2a1f146876
arnandofirhan <arnandofp@gmail.com>
2026-09-11 03:14:04 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -10,7 +10,7 @@
  * dan perubahannya tidak muncul di HP, naikkan CACHE_VERSION di bawah
  * ini supaya service worker lama dibuang & cache diisi ulang.
  *************************************************************/
-var CACHE_VERSION = 'agrinesia-b2b-v9';
+var CACHE_VERSION = 'agrinesia-b2b-v10';
 var APP_SHELL = [
   './',
   './index.html',
